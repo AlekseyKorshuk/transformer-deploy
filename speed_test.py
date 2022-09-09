6dc809e2709e2968039eb7865cf7aa89fff6574d
Aleksey Korshuk <ale-kor02@mail.ru>
2022-09-09 22:07:55 +0300

    updates

--- speed_test.py
+++ speed_test.py
@@ -14,8 +14,8 @@ import torch
 from onnxruntime import ExecutionMode, GraphOptimizationLevel, InferenceSession, IOBinding, OrtValue, SessionOptions
 
 # model = ORTModelForCausalLM.from_pretrained(save_directory, file_name="model-quantized.onnx")
-from transformer_deploy.backends.ort_utils import create_model_for_provider, torch_to_numpy_dtype_dict, to_pytorch
-
+from transformer_deploy.backends.ort_utils import create_model_for_provider, torch_to_numpy_dtype_dict, to_pytorch, \
+    inference_onnx_binding
 
 warnings.filterwarnings("ignore")
 dataset = load_dataset("ChaiML/user_model_inputs")
@@ -74,13 +74,30 @@ ort_model = ORTModelForCausalLM.from_pretrained("gpt2", from_transformers=True)
 ort_model.to(torch.device("cuda"))
 
 
-ds_model = deepspeed.init_inference(
-    model=model,  # Transformers models
-    mp_size=1,  # Number of GPU
-    dtype=torch.float16,  # dtype of the weights (fp16)
-    replace_method="auto",  # Lets DS autmatically identify the layer to replace
-    replace_with_kernel_inject=True,  # replace the model with the kernel injector
-)
+class InferenceSessionWithIOBinding(InferenceSession):
+    def __init__(self, model_path, provider, nb_threads=1):
+        self.ort_model = create_model_for_provider(
+            path=model_path,
+            provider_to_use=provider,
+            nb_threads=nb_threads,
+        )
+
+    def run(self, output_names, input_feed, run_options=None):
+        for key in input_feed.keys():
+            input_feed[key] = torch.tensor(input_feed[key], device=0)
+        results = inference_onnx_binding(model_onnx=self.ort_model, inputs=input_feed, device="cuda")
+        logits = results["logits"]
+        output = logits.cpu().numpy()
+        return [output]
+
+
+model_path = "onnx-gpt2/model.onnx"
+provider = "CUDAExecutionProvider"
+nb_threads = 1
+
+engine = InferenceSessionWithIOBinding(model_path=model_path, provider=provider, nb_threads=nb_threads)
+
+ort_model.model = engine
 
 # create acclerated pipeline
 # ds_clf = pipeline("text-generation", model=ds_model, tokenizer=tokenizer, device=0)
@@ -89,14 +106,14 @@ print("Accelerated single batch")
 accelerated_outputs = []
 for example in tqdm.tqdm(INPUT_EXAMPLES[:20], desc="Accelerated single batch"):
     inputs = tokenizer(example, return_tensors='pt').to(0)
-    result = ds_model.generate(**inputs, **GENERATION_KWARGS)
+    result = ort_model.generate(**inputs, **GENERATION_KWARGS)
 
 print("Accelerated batch size")
 accelerated_outputs = []
 try:
     for example in tqdm.tqdm(INPUT_EXAMPLES[:10], desc="Accelerated batch size"):
         inputs = tokenizer([example] * max_batch_size, return_tensors='pt').to(0)
-        result = ds_model.generate(**inputs, **GENERATION_KWARGS)
+        result = ort_model.generate(**inputs, **GENERATION_KWARGS)
 except Exception as ex:
     print(ex)
 # accelerated_output = ds_clf(example, **GENERATION_KWARGS)[0]["generated_text"][len(example):]
