0a8ed070ec8b4c87128b75ae748da47eb064bb16
Aleksey Korshuk <ale-kor02@mail.ru>
2022-09-10 15:09:43 +0300

    updates

---
+++ onnx_model.py
@@ -0,0 +1,20 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+
+class OnnxModel(AutoModelForCausalLM):
+
+    # def __init__(self):
+    #     pass
+
+    def _forward(self, **args):
+        print(args)
+        return None
+
+
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+inputs = tokenizer("Hello,", return_tensors="pt")
+model = OnnxModel.from_pretrained("gpt2")
+
+output = model(**inputs)
+
+print(output)
