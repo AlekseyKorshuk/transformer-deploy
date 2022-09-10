1daea9e0dc5cd604d2a31dbc2617f2056572101b
Aleksey Korshuk <ale-kor02@mail.ru>
2022-09-10 15:28:06 +0300

    updates

--- onnx_model.py
+++ onnx_model.py
@@ -4,11 +4,13 @@ from generation_mixin import GenerationMixin
 config = AutoConfig.from_pretrained("gpt2")
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
 inputs = tokenizer("Hello,", return_tensors="pt")
-model = AutoModelForCausalLM.from_config(config)
+model = AutoModelForCausalLM.from_config(config).to(0)
 
 print(model.__dict__)
+input("1")
 del model._modules
 model._modules = None
+input("1")
 # mixin = GenerationMixin()
 print(type(model))
 output = model(**inputs)
