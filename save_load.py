1168317d796d6d8946a6a0691b3c9e9941aa2fe4
Aleksey Korshuk <ale-kor02@mail.ru>
2022-09-11 14:16:16 +0300

    updates

--- save_load.py
+++ save_load.py
@@ -3,4 +3,4 @@ import onnx, onnxruntime
 model_name = '/model-storage/onnx-lit-v2-with-past/model.onnx'
 onnx_model = onnx.load(model_name)
 
-onnx.save(onnx_model, "test-model/model.onnx")
+onnx.save(onnx_model, "test-model/model.onnx", save_as_external_data=True)
