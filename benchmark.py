707243ebc4135de258f8f75354e873bbbd2afce9
Aleksey Korshuk <ale-kor02@mail.ru>
2022-09-13 14:38:26 +0300

    updates

--- benchmark.py
+++ benchmark.py
@@ -8,6 +8,10 @@ from transformer_deploy.backends.ort_utils import create_model_for_provider, inf
 from transformer_deploy.benchmarks.utils import generate_multiple_inputs
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import load_dataset
+from inference_service import ONNXWrapper
+import os
+from transformers import AutoConfig, AutoTokenizer
+import time
 
 from transformer_deploy.convert import check_accuracy
 
@@ -15,21 +19,14 @@ dataset = load_dataset("ChaiML/user_model_inputs")
 
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
 
-model_path = "./triton_models/model.onnx"
-model_path = "./onnx-lit/model.onnx"
-provider = "CUDAExecutionProvider"
-nb_threads = 1
-ort_model = create_model_for_provider(
-    path=model_path,
-    provider_to_use=provider,
-    nb_threads=nb_threads,
-)
+MODEL_ID = "hakurei/litv2-6B-rev2"
+MODEL_PATH = "/model-storage/onnx-lit-v2-with-past-new"
+FP16_MODEL_FILENAME = "model_fp16.onnx"
+MODEL_FILENAME = "model.onnx"
 
 
-def infer_ort(inputs: Dict[str, torch.Tensor]) -> torch.Tensor:
-    results = inference_onnx_binding(model_onnx=ort_model, inputs=inputs, device="cuda")
-    return results
-    # return results["output"] if "output" in results else (results["start_logits"], results["end_logits"])
+config = AutoConfig.from_pretrained(MODEL_ID)
+fp16_model = ONNXWrapper(os.path.join(MODEL_PATH, FP16_MODEL_FILENAME), config)
 
 
 device = 0
@@ -43,15 +40,15 @@ for i in tqdm.tqdm(X):
     # attention_mask = torch.tensor([[i] * i] * batch_size, dtype=torch.int64).to(device)
     inputs = tokenizer(i, return_tensors="pt").to(0)
     start_time = time.time()
-    result = infer_ort({"input_ids": inputs.input_ids, "attention_mask": inputs.attention_mask})["logits"].detach()
+    result = fp16_model(**inputs)
     # data = onnx_model(input_ids=input_ids, attention_mask=attention_mask)
     duration = time.time() - start_time
     Y_onnx.append(duration)
-    onnx_outputs.append(result)
+    # onnx_outputs.append(result)
 # print(result)
-print(result.size())
+# print(result.size())
 
-del ort_model
+del fp16_model
 
 torch_model = AutoModelForCausalLM.from_pretrained("hakurei/litv2-6B-rev2").to(0)
 Y_torch = []
@@ -62,12 +59,12 @@ with torch.no_grad():
         # attention_mask = torch.tensor([[i] * i] * batch_size).to(device)
         inputs = tokenizer(i, return_tensors="pt").to(0)
         start_time = time.time()
-        result = torch_model(**inputs).logits.detach()
+        result = torch_model.generate(**inputs)
         duration = time.time() - start_time
         Y_torch.append(duration)
-        torch_outputs.append(result)
+        # torch_outputs.append(result)
 # print(result)
-print(result.size())
+# print(result.size())
 
 plt.plot(list(range(len(X))), Y_torch, label="torch")
 plt.plot(list(range(len(X))), Y_onnx, label="onnx")
--- onnx_model_generate.py
+++ onnx_model_generate.py
@@ -10,6 +10,10 @@ from datasets import load_dataset
 import tqdm
 import time
 import matplotlib.pyplot as plt
+from inference_service import ONNXWrapper
+import os
+from transformers import AutoConfig, AutoTokenizer
+import time
 
 GENERATION_KWARGS = {
     "max_new_tokens": 64,
@@ -23,28 +27,7 @@ GENERATION_KWARGS = {
     'repetition_penalty': 1.13,
 }
 
-config = AutoConfig.from_pretrained("gpt2")
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
-inputs = tokenizer("Hello,", return_tensors="pt").to(0)
-model = AutoModelForCausalLM.from_config(config)
-
-
-class InferenceSessionWithIOBinding(InferenceSession):
-    def __init__(self, model_path, provider, nb_threads=1):
-        self.ort_model = create_model_for_provider(
-            path=model_path,
-            provider_to_use=provider,
-            nb_threads=nb_threads,
-        )
-
-    def run(self, output_names, input_feed, run_options=None):
-        for key in input_feed.keys():
-            input_feed[key] = torch.tensor(input_feed[key], device=0)
-        results = inference_onnx_binding(model_onnx=self.ort_model, inputs=input_feed, device="cuda")
-        logits = results["logits"]
-        # output = logits.cpu().numpy()
-        return logits
-
 
 dataset = load_dataset("ChaiML/user_model_inputs")
 
@@ -53,27 +36,25 @@ X = dataset["train"]["text"][:10]
 model_path = "onnx-lit/model.onnx"
 provider = "CUDAExecutionProvider"
 nb_threads = 1
+MODEL_ID = "hakurei/litv2-6B-rev2"
+MODEL_PATH = "/model-storage/onnx-lit-v2-with-past-new"
+FP16_MODEL_FILENAME = "model_fp16.onnx"
+MODEL_FILENAME = "model.onnx"
 
-onnx_model = InferenceSessionWithIOBinding(model_path=model_path, provider=provider, nb_threads=nb_threads)
-
-mixin = GenerationMixin(
-    model=model,
-    onnx_model=onnx_model
-)
+config = AutoConfig.from_pretrained(MODEL_ID)
+fp16_model = ONNXWrapper(os.path.join(MODEL_PATH, FP16_MODEL_FILENAME), config)
 
 Y_onnx = []
 onnx_outputs = []
 for i in tqdm.tqdm(X):
     inputs = tokenizer(i, return_tensors="pt").to(0)
     start_time = time.time()
-    output = mixin.generate(**inputs, **GENERATION_KWARGS)
+    output = fp16_model.generate(**inputs, **GENERATION_KWARGS)
     duration = time.time() - start_time
     Y_onnx.append(duration)
     onnx_outputs.append(tokenizer.decode(output[0])[len(i):])
 
-del onnx_model
-del mixin.onnx_model
-del mixin
+del fp16_model
 
 torch_model = AutoModelForCausalLM.from_pretrained("hakurei/litv2-6B-rev2").to(0)
 Y_torch = []
