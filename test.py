fdb43b8b3c7d8a77b29bd72ac94de430e54229cd
Aleksey Korshuk <ale-kor02@mail.ru>
2022-09-13 12:57:42 +0300

    updates

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-from onnxmltools.utils.float16_converter import convert_float_to_float16
+from onnxmltools.utils.float16_converter import convert_float_to_float16, convert_float_to_float16_model_path
 from onnxmltools.utils import load_model, save_model
 
-onnx_model = load_model('model.onnx')
-new_onnx_model = convert_float_to_float16(onnx_model)
+# onnx_model = load_model('model.onnx')
+new_onnx_model = convert_float_to_float16_model_path('model.onnx')
 save_model(new_onnx_model, 'model_fp16.onnx')
